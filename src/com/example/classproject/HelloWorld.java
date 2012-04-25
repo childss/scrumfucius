04428750b829bac386bbf7182a414ebbf337496d
Stuart Childs <childss@codecavern.org>
2012-04-25 16:14:02 -0400

    read from file and print to standard out

--- src/com/example/classproject/HelloWorld.java
+++ src/com/example/classproject/HelloWorld.java
@@ -1,8 +1,28 @@
 package com.example.classproject;
 
+import java.io.*;
+
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		System.out.println("Arrrggghghhhghhh....");
+    static String _tale;
+
+	public static void main(String[] args) throws IOException {
+        String filename = args[0];
+        File storyFile = new File(filename);
+
+        readFile(storyFile);
+		System.out.print(_tale);
+        System.out.println();
 	}
+
+    public static String readFile(File inputFile) throws FileNotFoundException, IOException {
+        if (!inputFile.exists()) { throw new FileNotFoundException(); }
+
+        FileReader fileReader = new FileReader(inputFile);
+        BufferedReader reader = new BufferedReader(fileReader);
+
+        _tale = reader.readLine();
+        return _tale;
+    }
+
 }
---
+++ src/com/example/classproject/HelloWorldTest.java
@@ -0,0 +1,54 @@
+package com.example.classproject;
+
+import org.junit.Test;
+
+import java.io.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testThrowsFileNotFound() {
+        try {
+            File textFile = new File("a file that does not exist");
+
+            HelloWorld.readFile(textFile);
+
+            fail("It didn't throw!");
+        } catch (FileNotFoundException e) {
+
+        } catch (Exception e) {
+            fail("Unexpected: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testReadsATextFile() {
+        File textFile = new File("dummy.txt");
+        try {
+            String output = HelloWorld.readFile(textFile);
+            assertEquals("this is some example text", output);
+        } catch (Exception e) {
+            fail("exception!: " + e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void testMainPrintsOutFile() {
+        String[] args = new String[1];
+        args[0] = "dummy.txt";
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        try {
+            HelloWorld.main(args);
+            assertEquals("this is some example text\n", output.toString());
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+}
