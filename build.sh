f065845df44eda233c1d48545f05febbb054226b
Katie Gengler <katiegengler@gmail.com>
2012-04-27 14:08:45 -0400

    fix build script

--- build.sh
+++ build.sh
@@ -8,13 +8,13 @@ if [ $? -ne 0 ]
 	then echo "build failed!";exit -1
 fi
 
-cp -r story /Volumes/AGILE\ BLUE/
-cp classproject.jar  /Volumes/AGILE\ BLUE/
-cp commons-lang-2.6.jar /Volumes/AGILE\ BLUE/
-cp scrumfucius  /Volumes/AGILE\ BLUE/
+cp -r story /Volumes/AGILE\ BLUE\ 2/
+cp classproject.jar  /Volumes/AGILE\ BLUE\ 2/
+cp commons-lang-2.6.jar /Volumes/AGILE\ BLUE\ 2/
+cp scrumfucius  /Volumes/AGILE\ BLUE\ 2/
 
-chmod 755 /Volumes/AGILE\ BLUE/scrumfucius
+chmod 755 /Volumes/AGILE\ BLUE\ 2/scrumfucius
 
-/Volumes/AGILE\ BLUE/scrumfucius
+/Volumes/AGILE\ BLUE\ 2/scrumfucius
 
 exit 0
